ad56bee652e4cd5dc8b5b5895971b95078ce9a72
Luke Videckis <lukevideckis@gmail.com>
2023-08-17 21:26:09 -0600

    better nav bar visibility

--- navagation_bar.js
+++ navagation_bar.js
@@ -1,3 +1,4 @@
-document.write('<ul>')
-document.write('    <li><a href="/index.html">Home</a></li>')
-document.write('</ul>')
+document.write('<div class="nav_bar">')
+document.write('    <a href="/index.html">Home</a>')
+document.write('</div>')
+document.write('</br>')
