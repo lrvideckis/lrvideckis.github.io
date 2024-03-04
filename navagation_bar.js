3da13ce40665ebdf641341ebeadebc75a227f1cc
Luke Videckis <lukevideckis@gmail.com>
2024-03-04 12:07:45 -0600

    tabs

--- navagation_bar.js
+++ navagation_bar.js
@@ -1,4 +1,4 @@
 document.write('<div class="nav_bar">')
-document.write('    <a href="/index.html">Home</a>')
+document.write('	<a href="/index.html">Home</a>')
 document.write('</div>')
 document.write('</br>')
